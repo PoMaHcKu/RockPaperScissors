23e9aacc736bc5e739d21c7d657354a3ac96127b
roman <rom2517pop@gmail.com>
2020-06-08 16:52:07 +0300

    Created hmac creator

--- src/main/java/by/itra/tasks/task3/utils/HmacCreator.java
+++ src/main/java/by/itra/tasks/task3/utils/HmacCreator.java
@@ -10,18 +10,35 @@ import java.util.Formatter;
 
 public class HmacCreator {
 
-    private final static String HMAC_SHA_3_256 = "HmacSHA3-256";
+    private final static String HMAC_SHA_2_256 = "HmacSHA256";
+    private final static int SIZE_KEY = 16;
+    private String key;
 
-    private byte[] getRandomKey(int countByte) {
-        SecureRandom random = new SecureRandom();
-        return random.generateSeed(countByte);
+    public String getHmac(String data) {
+       return calculateHMAC(data);
     }
 
-    private String calculateHMAC(String data, byte[] key) {
-        SecretKey secretKey = new SecretKeySpec(key, HMAC_SHA_3_256);
+    public String getKey() {
+        return key;
+    }
+
+    private void initRandomKey(int countByte) {
+        SecureRandom random = new SecureRandom();
+        byte[] seed = random.generateSeed(countByte);
+
+        Formatter formatter = new Formatter();
+        for (byte b : seed) {
+            formatter.format("%02X", b);
+        }
+        key = formatter.toString();
+    }
+
+    private String calculateHMAC(String data) {
+        initRandomKey(SIZE_KEY);
+        SecretKey secretKey = new SecretKeySpec(key.getBytes(), HMAC_SHA_2_256);
         Mac mac = null;
         try {
-            mac = Mac.getInstance(HMAC_SHA_3_256);
+            mac = Mac.getInstance(HMAC_SHA_2_256);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
